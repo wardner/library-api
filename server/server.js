4125e76a33e8d2704fbd6eda4097beee39daf4dc
Wardner <wanerlara1999@gmail.com>
2020-03-24 16:43:21 -0400

    Paquetes actualizados

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(require('./controllers/book'));
 app.use(require('./controllers/sheet'));
